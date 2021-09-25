08a14e91289887a57c4da0794a6fe3a0ea24c0ca
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-09-25 13:01:11 +0700

    fix(kontak): adding kontak page

--- src/views/Kontak.vue
+++ src/views/Kontak.vue
@@ -3,19 +3,81 @@
     <div class="container-fluid">
       <HomeNavbar />
       <div class="row mt-5">
-        <h3>Kontak</h3>
+        <div class="col-md-7">
+          <h3>Kontak Kami</h3>
+          <p>
+            Konsultasikan kebutuhan anda kepada kami, kami ada melalui berbagai
+            media komunikasi. Kabari kami, kami akan hubungi anda.
+          </p>
+          <input type="text" class="form-control mb-3" placeholder="Nama" />
+          <input type="text" class="form-control mb-3" placeholder="Email" />
+          <textarea
+            id=""
+            cols="30"
+            rows="10"
+            class="form-control mb-3"
+            placeholder="Pesan"
+          ></textarea>
+        </div>
+        <div class="col-md-5 mt-5">
+          <div
+            class="form-contact d-flex flex-column align-items-between justify-content-center bg-primary p-5 text-white"
+          >
+            <div class="hotline flex-grow-1 mb-5">
+              <label for="">Hotline</label>
+              <h2>0812-1543-4700</h2>
+            </div>
+            <div class="email flex-grow-1 mb-3">
+              <label>Email</label>
+              <h5>ppt.uii@uii.ac.id</h5>
+            </div>
+            <div class="instagram flex-grow-1 mb-3">
+              <label>Instagram</label>
+              <h5>@pptuii</h5>
+            </div>
+            <div class="alamat flex-grow-1 mb-3">
+              <label>Alamat</label>
+              <h5>
+                Jl. Demangan Baru No.24, Mrican, Caturtunggal, Kec. Depok,
+                Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281
+              </h5>
+            </div>
+            <div class="alamat flex-grow-1 mb-5">
+              <label>Hours</label>
+              <h5>
+                Senin – Jumat: 10.00 – 16.00 <br />
+                Sabtu – Ahad: Tutup
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HomeNavbar from '@/components/Home/Navbar'
+import HomeNavbar from "@/components/Home/Navbar";
 
 export default {
-  name: 'Kontak',
+  name: "Kontak",
   components: {
     HomeNavbar,
-  }
-}
+  },
+};
 </script>
+
+<style scoped>
+.left {
+  max-width: 600px;
+}
+
+.right {
+  max-width: 600px;
+}
+
+.bg-primary {
+  background-color: #8E546E !important;
+  opacity: 0.8;
+}
+</style>
